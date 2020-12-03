6ce38c86c008f14230848875c656c5ae144c73ff
pavel_sol <pavel.solov@inbox.ru>
2020-12-03 17:38:33 +0400

    написал скрипт

--- js/script.js
+++ js/script.js
@@ -1,17 +1,33 @@
-// ==================================================================
-let btn = document.querySelector('#btn')
+let amountInput = document.querySelector('.input__amount')
+let fromInput = document.querySelector('.input__from')
+let toInput = document.querySelector('.input__to')
 
-btn.addEventListener('click', start)
+let outOne = document.querySelector('.out__one')
+let outAllFrom = document.querySelector('.out__all__from')
+let outAllto = document.querySelector('.out__all__to')
+
+amountInput.addEventListener('input', start)
+fromInput.addEventListener('input', start)
+toInput.addEventListener('input', start)
+
 
 function start() {
-    fetch('https://api.exchangeratesapi.io/latest?base=USD')
+    fetch(`https://api.exchangeratesapi.io/latest?base=${fromInput.value}`)
         .then((data) => {
-            // console.log(data.text())
             return data.json()
         })
         .then((data) => {
-            console.log(data.rates.RUB)
-            console.log(data)
+            outInfo(data)
         })
 }
+
+function outInfo(data) {
+    let amountСurrency = (data.rates[toInput.value]).toFixed(2)
+
+    outOne.textContent = `1 ${fromInput.value} = ${amountСurrency} ${toInput.value}`
+
+    outAllFrom.textContent = `${amountInput.value} ${fromInput.value} = `
+    outAllto.textContent = `${(amountСurrency * amountInput.value).toFixed(2)} ${toInput.value}`
+}
+
 // ===================================================================================
